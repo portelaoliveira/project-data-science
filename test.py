7093711060653539e24e0f53cdaffe992acc4902
Danilo Portela <daniloportela97@gmail.com>
2023-08-14 16:18:13 -0300

    Importar e consolidar as bases de dados.

--- test.py
+++ test.py
@@ -8,3 +8,38 @@ from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 from sklearn.model_selection import train_test_split
+
+months = {
+    "jan": 1,
+    "fev": 2,
+    "mar": 3,
+    "abr": 4,
+    "mai": 5,
+    "jun": 6,
+    "jul": 7,
+    "ago": 8,
+    "set": 9,
+    "out": 10,
+    "nov": 11,
+    "dez": 12,
+}
+
+path_dataset = Path("dataset")
+
+data_base_airbnb = pd.DataFrame()
+
+for file in path_dataset.iterdir():
+    month_name = file.name[:3]
+    month = months[month_name]
+
+    year = file.name[-8:]
+    year = int(year.replace(".csv.xz", ""))
+
+    df = pd.read_csv(path_dataset / file.name, low_memory=False)
+    df["ano"] = year
+    df["mes"] = month
+
+    data_base_airbnb = pd.concat([df, data_base_airbnb], ignore_index=True)
+
+
+print(data_base_airbnb)
